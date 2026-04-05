f630220ca4a03aa3c097760fc9bc485760e503b2
AdityaPandeyCN <adityapand3y666@gmail.com>
2026-04-05 11:15:43 +0530

    changes
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,9 @@ using OptimizationBBO, OptimizationOptimJL
 
 ##
 
-# Create test suite for dimension 3
 const D = 3
 test_functions = bbob_suite(Val(D))
 
-# Test that functions evaluate correctly at optimum
 @testset "Function optima" begin
     for f in test_functions
         @test f(f.x_opt) ≈ f.f_opt atol=1e-5
@@ -21,25 +19,24 @@ end
 ##
 
 b = BBOB.benchmark(
-    NelderMead(), test_functions[1], [100, 500, 1000],
+    NelderMead(), test_functions[1], [100, 500, 1000], 
 )
 
 b = BBOB.benchmark(
-    BenchmarkSetup(NelderMead(); isboxed=false), test_functions[1], [100, 500, 1000],
+    BenchmarkSetup(NelderMead(); isboxed=false), test_functions[1], [100, 500, 1000], 
 )
 
 @test length(b.success_count) == 3
 
-b = BBOB.benchmark(
-    NelderMead(), test_functions[1:3], 100:100:2000, Ntrials=10,
+b1 = BBOB.benchmark(
+    NelderMead(), test_functions, [100, 500, 1000],
 )
 
 b2 = BBOB.benchmark(
-    ParticleSwarm(), test_functions[1:3], 100:200:2000, Ntrials=10,
+    ParticleSwarm(), test_functions, [100, 500, 1000],
 )
 
-BBOB.compute_CI!(b, 0.1)
-plot(b; title = "Benchmark", label = "NelderMead", legend = :outerright)
+plot(b1; label = "NelderMead")
 plot!(b2; label = "ParticleSwarm")
 
 ## OptimizationBBO
@@ -47,11 +44,18 @@ plot!(b2; label = "ParticleSwarm")
 setup = Chain(
     BenchmarkSetup(BBO_adaptive_de_rand_1_bin(), isboxed = true),
     BenchmarkSetup(NelderMead(), isboxed = false),
-    0.9
+    0.9,
 )
 
 b = BBOB.benchmark(
-    setup, test_functions[1:2], 100:200:5000, Ntrials=10,
+    setup, test_functions, [1000, 5000, 10000],
 )
 
 plot(b)
+
+##
+
+plot_functions = bbob_suite(Val(2))
+plot(plot_functions[1])
+
+##
